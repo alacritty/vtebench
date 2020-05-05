a6c184ff35d35463bcb70f14adc2ae6a3b448812
Kirill Chibisov <contact@kchibisov.com>
2020-05-06 01:05:33 +0300

    Fix generation

--- src/bench.rs
+++ src/bench.rs
@@ -43,8 +43,8 @@ pub fn alt_screen_random_write<W: Write>(ctx: &mut Context<W>, options: &Options
 
     while written < options.bytes {
         let (l, c) = (rng.gen_range(0, h), rng.gen_range(0, w - 1));
-        let space = w - c + 1;
-        let to_write = rng.gen_range(0, space);
+        let space = w - c;
+        let to_write = rng.gen_range(0, space) + 1;
 
         written += ctx.cup(l, c)?;
         if options.colorize {
@@ -129,7 +129,7 @@ pub fn unicode_random_write<W: Write>(ctx: &mut Context<W>, options: &Options) -
     let w = options.width;
 
     while written < options.bytes {
-        let (l, c) = (rng.gen_range(0, h), rng.gen_range(0, w));
+        let (l, c) = (rng.gen_range(0, h), rng.gen_range(0, w - 1) + 1);
 
         written += ctx.cup(l, c)?;
         if options.colorize {
