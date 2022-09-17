c75155bfc252227c0efc101c1971df3e327c71c4
Christian Duerr <contact@christianduerr.com>
2022-09-17 15:17:14 +0200

    Migrate to clap 3

--- src/cli.rs
+++ src/cli.rs
@@ -2,36 +2,36 @@
 
 use std::path::PathBuf;
 
-use structopt::StructOpt;
+use clap::Parser;
 
-#[derive(StructOpt)]
-#[structopt(author)]
+#[derive(Parser)]
+#[clap(author)]
 pub struct Config {
     /// Do not print results to stdout.
-    #[structopt(short, long)]
+    #[clap(short, long)]
     pub silent: bool,
 
     /// Benchmark source directories.
-    #[structopt(short, long, value_name = "DIRECTORY", default_value = "./benchmarks")]
+    #[clap(short, long, value_name = "DIRECTORY", default_value = "./benchmarks")]
     pub benchmarks: Vec<PathBuf>,
 
     /// Number of warmup iterations.
-    #[structopt(long, value_name = "NUM", default_value = "1")]
+    #[clap(long, value_name = "NUM", default_value = "1")]
     pub warmup: usize,
 
     /// Minimum number of bytes per benchmark sample.
-    #[structopt(long, value_name = "BYTES", default_value = "1048576")]
+    #[clap(long, value_name = "BYTES", default_value = "1048576")]
     pub min_bytes: usize,
 
     /// Maximum number of samples per benchmark.
-    #[structopt(long, value_name = "NUM")]
+    #[clap(long, value_name = "NUM")]
     pub max_samples: Option<usize>,
 
     /// Maximum number of seconds per benchmark.
-    #[structopt(long, value_name = "SECONDS", default_value = "10")]
+    #[clap(long, value_name = "SECONDS", default_value = "10")]
     pub max_secs: u64,
 
     /// Output results to a Gnuplot compatible DAT file.
-    #[structopt(long, value_name = "FILE")]
+    #[clap(long, value_name = "FILE")]
     pub dat: Option<PathBuf>,
 }
